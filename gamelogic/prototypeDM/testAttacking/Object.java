d732a280e1ca29b739b5534c563a9905c74372c7
team4jevo <team4jevo@gmail.com>
2016-08-17 16:52:20 +0300

    Create Object.java

---
+++ gamelogic/prototypeDM/testAttacking/Object.java
@@ -0,0 +1,32 @@
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Object {
+	private int power;
+	// int x;
+	// int y;
+	private boolean isAlive = false;
+
+	int min = 1;
+	int max = 10;
+
+	public Object(boolean isAlive) {
+		power = ThreadLocalRandom.current().nextInt(min, max + 1);
+		this.isAlive = isAlive;
+	}
+
+	public int getPower() {
+		return power;
+	}
+
+	public void setPower(int power) {
+		this.power = power;
+	}
+
+	public boolean isAlive() {
+		return isAlive;
+	}
+
+	public void setAlive(boolean isAlive) {
+		this.isAlive = isAlive;
+	}
+}
