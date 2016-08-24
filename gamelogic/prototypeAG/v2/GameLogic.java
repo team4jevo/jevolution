64a49c347501932005a931e91adc004e779f90c3
Aigars Gridjusko <aigars.gridjusko@gmail.com>
2016-08-24 15:05:25 +0300

    Working custom game logic. LocalCreature made public

--- gamelogic/prototypeAG/v2/GameLogic.java
+++ gamelogic/prototypeAG/v2/GameLogic.java
@@ -6,28 +6,6 @@ import java.util.Random;
  * Created by LongJohn on 8/19/2016.
  */
 
-/*
- * interface this class should have following stuff methods Constructor
- * (GraphicEngine ge) should take in pointer to graphic engine object game
- * engine usage examples field [x][y] = new GameObject("f",x,y); field
- * [x][y].setToken (ge.createToken (field [x][y])); GameObject.setToken connects
- * game object ( creature food etc in logical simulation) with token -
- * representation on UI level each game object that is to be displayed in UI
- * have to be connected to a token game object and token have pointers to each
- * other, that allows them to call methods in both deirections field
- * [x][y].getToken().move(1, -1); field [x][y].move(1, -1); this can be
- * rewritten ( move method can be added to GameObject move method moves token to
- * new position
- * 
- * field [x][y].removeToken(); removes token is not done yet note that in
- * GameLogic everywhere x and y are Logical coordinates, not graphical in pixels
- * 
- * newGame () should start new game (currently on newGame remains of previous
- * nextTurn () controller calls it each time when new turn is needed amount of
- * actions made in one turn is up to a GameLogic - it can be single action of
- * one organism or processing all of the organisms or anything else
- * 
- */
 
 public class GameLogic extends jevo.GameLogic {
     int tmpX = 0;
@@ -195,7 +173,7 @@ public class GameLogic extends jevo.GameLogic {
 
     public void newGame() throws Exception {
         // Game settings
-        initializeGrid(15, 15, "default");
+        initializeGrid(30, 30, "default");
         CreatureSimple.setAggressiveness(1);
         CreatureDependant.setAggressiveness(2);
         CreatureDependant.setCooperativeness(4);
--- gamelogic/prototypeAG/v2/LocalCreature.java
+++ gamelogic/prototypeAG/v2/LocalCreature.java
@@ -1,12 +1,12 @@
 package customgl;
 
 
-class LocalCreature extends jevo.GameObject {
+public class LocalCreature extends jevo.GameObject {
     static final double SIGMA = 0.01;
     static final double K = 0.03162277660168379;
     private boolean isAlive;
 
-    LocalCreature(String type, int x, int y) {
+    public LocalCreature(String type, int x, int y) {
         super (type, x, y);
         this.isAlive = true;
     }
