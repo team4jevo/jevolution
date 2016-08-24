e05ae87407cd4d1c884fbc056ef487ff7730d860
Aigars Gridjusko <aigars.gridjusko@gmail.com>
2016-08-24 15:58:55 +0300

    Adding metrics classes (work in progress)

---
+++ metrics/GameObjectDB.java
@@ -0,0 +1,16 @@
+package metrics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GameObjectDB {
+    private List<GameObjectRecord> records;
+    
+    public GameObjectDB() {
+        this.records = new ArrayList<>();
+    }
+    
+    public void addRecord(GameObjectRecord record) {
+        
+    }
+}
