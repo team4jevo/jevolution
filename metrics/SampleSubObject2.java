ba8c52ebc34e1c51f4aa9c5b2ef4641bdf94c097
team4jevo <team4jevo@gmail.com>
2016-08-27 09:45:20 +0300

    Delete SampleSubObject2.java

--- metrics/SampleSubObject2.java
+++
@@ -1,9 +0,0 @@
-package metrics;
-
-public class SampleSubObject2 extends SampleSuperObject {
-    public SampleSubObject2() throws IllegalArgumentException, IllegalAccessException {
-        super();
-    }
-
-    private int subInt;
-}
